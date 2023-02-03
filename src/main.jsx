2bb6465e3573d40957473cded80bbfeae1e741ff
alef-garrido <oa.g11@icloud.com>
2023-02-02 19:46:57 -0600

    Add pags folder, css brand resets

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './CounterApp';
+import { CounterApp } from './components/widgets/CounterApp';
 
 import './styles.css';
 
